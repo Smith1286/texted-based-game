c066d9640c2aaa014da61f61266cec9e5f9bafeb
Smith1286 <89523712+Smith1286@users.noreply.github.com>
2022-04-28 08:29:08 -0400

    Add files via upload

--- text_Adventure.py
+++ text_Adventure.py
@@ -4,7 +4,11 @@ directions = ["left", "right", "forward", "backward"]
  
 # Introduction
 name = input("Dio: What is your name, adventurer?\n")
+print("----------------------------------------------------------")
+print(" ")
 print("Dio: Greetings, " + name + ". Let us go on a adventure!")
+print(" ")
+print("-----------------------------------------------------------")
 print("You find yourself on the edge of a dark forest.")
 print("Can you find your way through?\n")
  
@@ -13,7 +17,10 @@ response = ""
 while response not in yes_no:
     response = input("Would you like to step into the forest?\nyes/no\n")
     if response == "yes":
+        print("----------------------------------------------------------------")
+        print(" ")
         print("You head into the forest. You hear crows cawwing in the distance.\n")
+        print("----------------------------------------------------------------")
     elif response == "no":
         print("You are not ready for this quest. Goodbye, " + name + ".")
         quit()
@@ -29,8 +36,12 @@ while response not in directions:
     response = input("What direction do you move?\nleft/right\n")
     if response == "left":
         print("")
+        print("----------------------------------------------------------------")
+        print(" ")
         print("You enter the dark cave and look around there is two ways to go.")
         print("Left or right")
+        print(" ")
+        print("----------------------------------------------------------------")
         print("")
     elif response == "right":
         print("")
@@ -40,7 +51,8 @@ while response not in directions:
         quit()
     else:
         print("I didn't understand that.\n")
-        
+# Next part of game
+       
 response = ""
 while response not in directions:
     print("")
@@ -54,29 +66,37 @@ while response not in directions:
         print("Thanks for playing " + name + ".")
         quit()
     elif response == "right":
-        print("")
-        print("You head into the lit up path and you reach a locked chest, you open it.")
+        print("----------------------------------------------------------------")
+        print(" ")
+        print("You head into the lit up path and you reach a locked chest and open it and find a golden arrow.")
+        print("You head out of the cave and see a town.")
+        print(" ")
+        print("----------------------------------------------------------------")
     else:
         print("")
         print("I didn't understand that.\n")
+# Next part of game
 
 response = ""
 while response not in directions:
     print("")
-    print("You open the chest you get a strange golden arrow.")
-    print("You then continue out the cave and reach a crossroad to the left is a bush and to the right is a town.")
+    print("You start heading toward the town and a hooded man is approaching you to the left.")
+    print("And to your right you see a bunch of bushes you can run too.")
     
-    response = input("What direction do you move?\nleft/right\n")  
-    if response == "left":
+    response = input("What direction do you move?\nleft/right\n")
+    if response == "right":
         print("")
-        print("You head into a bush")
-        print("On the other side of the bush is a cliff and you fall.")
+        print("You run away from the man and into the bushes and you are met with a cliff on the other side and you fall.")
         print("Thanks for playing " + name + ".")
         quit()
-    elif response == "right":
+    elif response == "left":
+        print("----------------------------------------------------------------")
         print("")
-        print("You head into town")
-        print("you meet a man wearing a jacket and hat he stops and talk with you and says don't go into the town.")
+        print("You start heading toward the hooded man and he stops you to ask you something about the golden arrow you found in the chest at the cave. ")
+        print("He say that the arrow is a dangerous tool and asks you to throw it off the cliff just near of there.")
+        print(" ")
+        print("----------------------------------------------------------------")
     else:
         print("")
         print("I didn't understand that.\n")
+ # Next part of game
